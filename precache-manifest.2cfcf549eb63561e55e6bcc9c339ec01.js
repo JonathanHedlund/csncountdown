b929d21d51e3721fd4003128b9a3e19c8a627c70
id16jhd <id16jhd@cs.umu.se>
2019-05-21 17:01:05 +0200

    Updates

---
+++ precache-manifest.2cfcf549eb63561e55e6bcc9c339ec01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51e86beedfa02e9120bc72b6898ec55b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4aa7e137c02730caffcd",
+    "url": "/static/css/main.3d74c8e5.chunk.css"
+  },
+  {
+    "revision": "7818d6152cc63b380dab",
+    "url": "/static/js/2.797b4d47.chunk.js"
+  },
+  {
+    "revision": "4aa7e137c02730caffcd",
+    "url": "/static/js/main.b0c97884.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
