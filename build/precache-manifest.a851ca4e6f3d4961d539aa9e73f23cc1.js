9374619618714bb2efd1cec9dfddc847b3c02fb0
id16jhd <id16jhd@cs.umu.se>
2019-05-23 10:53:33 +0200

    Added financial support amount and fixed some spacing

--- build/precache-manifest.a851ca4e6f3d4961d539aa9e73f23cc1.js
+++ build/precache-manifest.a851ca4e6f3d4961d539aa9e73f23cc1.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ca9bf8ab92822fc872edf44671901c0f",
+    "revision": "655d3b63b0feccd89d5b6bd45fe110c6",
     "url": "/index.html"
   },
   {
-    "revision": "133110d6d95fde6ac5a9",
-    "url": "/static/css/main.0518b904.chunk.css"
+    "revision": "3021806114fc7b8f26e6",
+    "url": "/static/css/main.bd4e3b74.chunk.css"
   },
   {
     "revision": "7818d6152cc63b380dab",
     "url": "/static/js/2.797b4d47.chunk.js"
   },
   {
-    "revision": "133110d6d95fde6ac5a9",
-    "url": "/static/js/main.b0c97884.chunk.js"
+    "revision": "3021806114fc7b8f26e6",
+    "url": "/static/js/main.024c81d3.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
--- src/Money.js
+++ src/Money.js
@@ -4,13 +4,14 @@ class Money extends Component {
     constructor() {
         super();
         this.state = {
-            money: "8 007"
+            money: "8 007",
+            support: "2 478"
         }
     }
     render() {
         return (
             <div>
-                <p>Studying full time (100%) will give you <em>{this.state.money}</em> SEK.</p>
+                <p>Studying full time (100%) will give you <em>{this.state.money}</em> SEK, where <em>{this.state.support}</em> SEK is financial support.</p>
             </div>
         )
 
