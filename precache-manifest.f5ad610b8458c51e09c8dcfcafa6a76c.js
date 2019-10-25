fefe84bcaf809969b3a88788addb22dd22e6d9aa
id16jhd <id16jhd@cs.umu.se>
2019-10-25 06:56:39 +0200

    Updates

---
+++ precache-manifest.f5ad610b8458c51e09c8dcfcafa6a76c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a94b5ed843a1e05c866fc1391d74702",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1312c8918cc88862508d",
+    "url": "/static/css/main.0abf0388.chunk.css"
+  },
+  {
+    "revision": "fdd9c1e0cc015c722b0c",
+    "url": "/static/js/2.c959c4aa.chunk.js"
+  },
+  {
+    "revision": "1312c8918cc88862508d",
+    "url": "/static/js/main.2c2e3b33.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "32cae961866ac8589332de71bc99b337",
+    "url": "/static/media/kazoo.32cae961.mp3"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a3018c3da60f4d1a1a67b33aae257bc2.js"
+  "/precache-manifest.f5ad610b8458c51e09c8dcfcafa6a76c.js"
 );
 
 self.addEventListener('message', (event) => {
